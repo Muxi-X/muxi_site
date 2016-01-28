881e09c4d5ded276fed56db8d63bc490ecbc1d4a
sunliying <1174587076@qq.com>
2016-01-28 18:45:59 +0800

    transplant the package.json file and gruntfile.js file to the blog derectory

--- muxiwebsite/blog/gruntfile.js
+++ muxiwebsite/blog/gruntfile.js
@@ -5,9 +5,9 @@ module.exports = function(grunt) {
         dynamic: {                         // Another target 
           files: [{
             expand: true,                  // Enable dynamic expansion 
-            cwd: '../src/img/',                   // Src matches are relative to this path 
+            cwd: './src/img/',                   // Src matches are relative to this path 
             src: ['*.{png,jpg,gif}'],   // Actual patterns to match 
-            dest: '../static/img/'                  // Destination path prefix 
+            dest: './static/img/'                  // Destination path prefix 
           }]
         }
       }
