99148327edd924d548523683afb93f09e3a59f2f
zxc0328 <120797887@qq.com>
2016-02-11 19:29:16 +0800

    add cookie for like

--- muxiwebsite/blog/static/js/main.js
+++ muxiwebsite/blog/static/js/main.js
@@ -1,14 +1,19 @@
 (function(){
+	var $likeBtn = $('svg.like');
+	var cookieFlag = Cookies.get('liked');
+	if (cookieFlag){
+		$likeBtn.parent().addClass("clicked");
+	}
 	// using regex to get the post id 
 	var pid = window.location.href.match(/post\/([0-9])+/)[1];
-	var $likeBtn = $('svg.like');
 	// flag, prevent from sending request more than once
 	var liked = false;
 	var url = '/api/blog/' + pid + '/likes/';
 	$likeBtn.click(function(){
-		if (liked) return;
+		if (liked || cookieFlag) return;
 		$.post(url).done(function(){
 			liked = !liked;
+			Cookies.set('liked', 'true', { expires: 7 });
 			$likeBtn.parent().addClass("clicked");
 		})
 	})
