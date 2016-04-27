41503dccc8e76ea9d6f1812496547bab2a7fec16
stephenliu <stephenlyao@yeah.net>
2016-04-27 11:13:40 +0800

    还书按钮、登录框bug

--- muxiwebsite/profile/static/js/main.js
+++ muxiwebsite/profile/static/js/main.js
@@ -1,33 +1,26 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /**
  * navigation for users page
  *
- * Powered by litejs
+ * 
  */
 'use strict';
 
-var index = (function appear() {
+(function() {
 
 	var btn = document.querySelector(".btn");
 	var pop = document.querySelector(".pop");
 	var cancel = document.querySelector(".cancel");
 
-	var click_on = btn.addEventListener('click', function () {
+	btn.addEventListener('click', function () {
 
 		pop.className = pop.className.replace(/hide/, ' ');
 		pop.className += ' appear ';
 	});
-	var click_off = cancel.addEventListener('click', function () {
+	cancel.addEventListener('click', function () {
 
 		pop.className = pop.className.replace(/appear/, ' ');
 		pop.className += ' hide ';
 	});
-
-	return {
-		click_on: click_on,
-		click_off: click_off
-	};
 })();
-index.click_on();
 
-},{}]},{},[1]);
+
