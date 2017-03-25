69821f33426c3452c9d7987b892ea18f19c82307
tangxiangru <871607149@qq.com>
2017-03-25 16:01:18 +0800

    commit

--- muxiwebsite/api/blog.py
+++ muxiwebsite/api/blog.py
@@ -9,7 +9,7 @@ from .forms import CommentForm
 from muxiwebsite import db, auth
 
 # 木犀博客首页
-@blogs.route('/',methods=['GET'])
+@blogs.route('/blog/',methods=['GET'])
 def index():
     page = int(request.args.get('page') or 1)
     article_tag = Tag.query.all()
@@ -25,86 +25,77 @@ def index():
         blog.intro = blog.summary
     article_date = []
 
-    for blog in blog_all:
-        if blog.index not in article_date:
-            article_date.append(blog.index)
+#    for blog in blog_all:
+#        if blog.index not in article_date:
+#            article_date.append(blog.index)
 
-    return jsonify({
-                   'blog_list':blog_list,
-                   'article_tag':article_tag,
-                   'article_date':article_date,
-                   'blog_avatar':blog_avatar,
-                   'blog_body':blog_body}) , 200
+    return jsonify([{
+                   'blog_tag':blog.tag.value,
+                   'blog_date':"%d/%02d/%02d" % (blog.timestamp.year, blog.timestamp.month, blog.timestamp.day),
+                   'blog_avatar':User.query.filter_by(id=blog.author_id).first().avatar_url,
+                   'blog_body':blog.body}
+                    for blog in blog_list]) , 200
 
 #博客归档页面return:
-@blogs.route('/index/<string:index>/', methods=["GET"])
+@blogs.route('/blog/index/<string:index>/', methods=["GET"])
 def ym(index):
-   
     blog_list = []
     for blog in Blog.query.all():
         if blog.index == index:
             blog_list.append(blog)
-    for blog in blog_list:
-        blog.date = "%d/%02d/%02d" % (blog.timestamp.year, blog.timestamp.month, blog.timestamp.day)
-        blog.avatar = User.query.filter_by(id=blog.author_id).first().avatar_url
-        blog.content = blog.body
-    article_date = []
+        article_date = []
     for blog in Blog.query.all():
         if blog.index not in article_date:
             article_date.append(blog.index)
-    return jsonify({
-                   'blog_list':blog.list,
+    return jsonify([{
                    'index':blog.index,
-                   'article_date':article.date
-                   'blog_avatar'=blog.avatar,
-                   'blog_content' = blog.body}) , 200
+                   'blog_date': "%d/%02d/%02d" % (blog.timestamp.year, blog.timestamp.month, blog.timestamp.day),
+                   'blog_avatar'=User.query.filter_by(id=blog.author_id).first().avatar_url,
+                   'blog_content' = blog.body}
+                    for blog in blog_list]) , 200
 
 
 #博客文章页面
-@blogs.route('/post/<int:id>/', methods=["POST", "GET"])
+@blogs.route('/blog/post/<int:id>/', methods=["GET"])
 def post(id):
     blog = Blog.query.get_or_404(id)
-    blog.content = blog.body
-    blog.date = "%d年%d月%d日 %d:%d" % (blog.timestamp.year,
-            blog.timestamp.month, blog.timestamp.day, blog.timestamp.hour,
-            blog.timestamp.minute)
-    if request.method == 'POST' :
-        # 提交评论
-        if current_user.is_authenticated:
-            name = current_user.username
-            uid = current_user.id
-        else:
-            name = form.username.data
-            uid = 0
-            comment = Comment()
-            comment.comment =request.get_json().get("comment")
-            comment.author_id= request.get_json().get("uid")
-            comment.author_name =  request.get_json().get("name")
-            comment.blog_id= request.get_json().get("id")
-
-            db.session.add(comment)
-            db.session.commit()
-
-            blog.comment_number += 1
-            db.session.add(blog)
-            db.session.commit()
-
-    return jsonify(comment.to_json()) , 201
-
     comment_list =Comment.query.filter_by(blog_id=id).all()
-    for comment in comment_list:
-        comment.date = str(comment.timestamp)[:-10]
-        comment.content = comment.comment
     return jsonify({
-                   'blog':blog.blog,
-                   'author_id':comment.author_id,
-                   'blog_id':comment.blog_id,
-                   'author_id':comment.author_name;
-                   'list':comment_list,
+                   'body':blog.body,
+                   'author_id':blog.author_id,
+                   'blog_id':blog.id,
+                   'blog_date': "%d年%d月%d日 %d:%d" % (blog.timestamp.year,
+                           blog.timestamp.month, blog.timestamp.day, blog.timestamp.hour,
+                           blog.timestamp.minute),
                    'comment': [comment.to_json() for comment in comment_list ]}) , 200
 
+@blogs.route('/blog/post/<int:id>/', methods=["POST"])
+def comment(id):
+    blog = Blog.query.get_or_404(id)
+    # 提交评论
+    if current_user.is_authenticated():
+        name = current_user.username
+        uid = current_user.id
+    else:
+        name = request.get_json().get("name")
+        uid = 0
+    comment = Comment()
+    comment.comment =request.get_json().get("comment")
+    comment.author_id= request.get_json().get("uid")
+    comment.author_name =  name
+    comment.blog_id= request.get_json().get("id")
+
+    db.session.add(comment)
+    db.session.commit()
+
+    blog.comment_number += 1
+    db.session.add(blog)
+    db.session.commit()
+    return 200
+
+
 #  返回对应分类下的文章,分类: WEB, 设计, 安卓, 产品, 关于
-@blogs.route('/type/<string:type>/',methods=['GET'])
+@blogs.route('/blog/type/<string:type>/',methods=['GET'])
 def types(type):
 
     page = int(request.args.get('page') or 1)
@@ -121,8 +112,12 @@ def types(type):
         if blog.index not in article_date:
             article_date.append(blog.index)
 
-    return jsonify({
-                   'blog_list':blog_list,
-                   'type_item':type_item,
-                   'page'=page,
-                   'article_date':article_date}) , 200
+    return jsonify([{
+                    'blog_tag':blog.tag.value,
+                    'blog_date':"%d/%02d/%02d" % (blog.timestamp.year, blog.timestamp.month, blog.timestamp.day),
+                    'blog_avatar':Use3r.query.filter_by(id=blog.author_id).first().avatar_url,
+                    'blog_body':blog.body
+
+                    'type_item':type_item,
+                    'page':page,}
+                    for blog in blog_list]) , 200
