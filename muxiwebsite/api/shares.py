4e24ec2a32a3b5f9352731193fd13525513ab38a
yuyilei <yyl1352091742@gmail.com>
2017-04-02 23:34:56 +0800

    fix share

--- muxiwebsite/api/shares.py
+++ muxiwebsite/api/shares.py
@@ -19,8 +19,9 @@ from . import api
 
 tags = ['frontend', 'backend', 'android', 'design', 'product']
 
-@api.route('/shares', methods=['GET'])
+@api.route('/shares/all/', methods=['GET'])
 def get_shares():
+
     """
     获取所有分享
     """
@@ -50,9 +51,11 @@ def get_shares():
     }), 200, {'link': '<%s>; rel="next", <%s>; rel="last"' % (next, last)}
 
 
-@api.route('/share/<int:id>/comments/',methods=["GET"])
+@api.route('/shares/<int:id>/comments/',methods=["GET"])
 def view_share(id) :
-    '''查看某一篇分享的评论'''
+    '''
+    查看某一篇分享的评论
+    '''
     share = Share.query.get_or_404(id) 
     share.author_id = Share.query.filter_by(id=id).first().author_id
     comments = Comment.query.filter_by(share_id=id).all()
@@ -71,18 +74,16 @@ def view_share(id) :
 
         }) ,200 
 
-@api.route('/add_comment/<int:id>',methods=['POST']) 
+@api.route('/shares/<int:id>/add_comment/',methods=['POST']) 
 def add_comment(id) :
-    share = Share.query.get_or_404(id)
-    share.author_id = Share.query.filter_by(id=id).first().author_id
     token = request.headers.get("token") 
     try :
         current_user_id = User.verify_auth_token(token).id
-    except :
+    except AttributeError :
         return jsonify({
-            
-            'message' : 'you can not add a comment!'
-            }) , 400 
+            'message':'You can not send a comment '
+            }),400 
+
     comment = Comment()
     comment.comment = request.get_json().get("comment")
     comment.share_id = id 
@@ -92,102 +93,112 @@ def add_comment(id) :
     db.session.commit()
 
     return jsonify({
-        
         'message' : 'You add a comment successfully!'
         }) , 200 
 
 
-@api.route('/send_share',methods=['POST'])
+@api.route('/shares/send/',methods=['POST'])
 def add_share() :
-    '''登录用户发送分享'''
-    share = Share() 
+    '''
+    登录用户发送分享
+    '''
     token = request.headers.get("token")
-    share.title =  request.get_json().get("title")
-    share.share = request.get_json().get("share")
-    share.tag = request.get_json().get("tag")
-    share.content = request.get_json().get("content")
-    current_user = User.verify_auth_token(token)
     try :
-        share.author_id = current_user.id
-    except :
+        current_user_id = User.verify_auth_token(token).id
+    except AttributeError :
         return jsonify({
-            
-            'message' : 'you can not send a share!'
-            }) , 200 
+            'message' : 'You can not send a share'
+            }) , 404 
+
+    share = Share()       
+    share.title =  request.get_json().get("title")
+    share.share = request.get_json().get("share") 
+    share.tag = request.get_json().get("tag")    
+    share.content = request.get_json().get("content")
+    share.author_id = current_user_id
+
     db.session.add(share)
     db.session.commit()
+
     return jsonify( { 
                     "share" : share.share ,
                     "title" : share.title ,    
                     "tag" : share.tag ,    
                     "author_id" : share.author_id ,
                     "id" :  share.id , 
-                    }
-               ,201 ,  {'Location': url_for('api.get_shares' , \
-                 _external=True)} ) 
- 
+                    } ) , 200 
 
-@api.route('/delete_share/<int:id>',methods=['DELETE'])
+@api.route('/shares/<int:id>/delete/',methods=['DELETE'])
 def delete(id) :
-    '''删除分享(发送分享的用户)'''
+    '''
+    删除分享(发送分享的用户)
+    '''
     share = Share.query.get_or_404(id)
     token = request.headers.get("token")
-    current_user_id = User.verify_auth_token(token).id
-    author_id = Share.query.filter_by(id=id).first().author_id
-    if  current_user_id == author_id :
-        db.session.delete(share)
-        db.session.commit()
+    try :
+        current_user_id = User.verify_auth_token(token).id
+    except AttributeError :
         return jsonify({
-            'deleted' : share.id  , 
+            'message': 'login first'
+            }) , 404 
 
-            }) , 200 
+    author_id = Share.query.filter_by(id=id).first().author_id
+    if  current_user_id != author_id :
+        return jsonify({
+            'message' : 'you can not delete it!'
+            }) , 404 
+
+    db.session.delete(share)
+    db.session.commit()
     return jsonify({
+            'deleted' : share.id  , 
+            }) , 200 
         
-        'message' : 'You can not delete it!'
-        }) , 400
 
-
-
-@api.route('/edit_share/<int:id>', methods=["PUT"])
+@api.route('/shares/<int:id>/edit/', methods=["PUT"])
 def edit(id) :
-    '''编辑已经发送的分享(发送该分享的用户)'''
+    '''
+    编辑已经发送的分享(发送该分享的用户)
+    '''
     share = Share.query.get_or_404(id) 
     token = request.headers.get("token")
-    current_user_id = User.verify_auth_token(token).id
-    author_id = Share.query.filter_by(id=id).first().author_id
-    if  current_user_id == author_id :
-        share.share = request.get_json().get("share")
-        share.title = request.get_json().get("title")
-        db.session.add(share)
-        db.session.commit()
+    try :
+        current_user_id = User.verify_auth_token(token).id
+    except AttributeError :
         return jsonify({
+            'message' : 'login first! '
+            }) , 400 
+
+    author_id = Share.query.filter_by(id=id).first().author_id
+    if  current_user_id != author_id :
+        return jsonify({
+            'message': 'You can not edit!'
+            }) , 404
+
+    share.share = request.get_json().get("share")
+    share.title = request.get_json().get("title")
+
+    db.session.add(share)
+    db.session.commit()
+    return jsonify({
             'edited' : share.id  ,
             }) , 200 
 
-    return jsonify({ 
-        
-        'message' : 'You can not edit!'
-        }) , 400 
 
-
-@api.route('/index_share',methods=['GET'])
+@api.route('/shares/',methods=['GET'])
 def index() :
-    '''分享首页,根据所选标签显示分享'''
+    '''
+    分享首页,根据所选标签显示分享
+    '''
     page = request.args.get('page',1,type=int) 
-    shares = {}
-
-    sort_arg = request.args.get("sort")
-    if sort_arg == None :
+    sort_args = request.args.get("sort")
+    if sort_args == None :
         shares_pages = \
         Share.query.order_by('-id').paginate(page,current_app.config['SHARE_PER_PAGE'],False)
         shares = shares_pages.items 
 
-    elif sort_arg == "new" :
-        shares_pages = Shares_pages = \
-        Share.query.order_by('-id').paginate(page,currnet_app.config['SHAER_PER_PAGE'],False)
-        shares = shares_pages.items
-
     elif sort_args == "hot" :
+        shares_count = {}
         shares = []
         for share in Share.query.all():
             shares_count[share] = share.comment.count()
@@ -199,9 +210,8 @@ def index() :
 
     elif sort_args in tags :
         shares = []
-        item = Share.query.filter_by(tag=sort_arg)
-        shares_pages = \
-        item.order_by('-id').pagniate(page,current_app.config['SHARE_PER_PAGE'],False)
+        item = Share.query.filter_by(tag=sort_args)
+        shares_pages = item.order_by('-id').paginate(page,current_app.config['SHARE_PER_PAGE'],False)
         shares = shares_pages.items 
 
     return jsonify({
