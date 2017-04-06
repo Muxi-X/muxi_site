a5b45d2e61a49d1a68510dcbb9402fb630475c0d
余某 <yyl1352091742@gmail.com>
2017-04-06 19:21:34 +0800

    Revert "Dev branch -share"

--- muxiwebsite/api/__init__.py
+++ muxiwebsite/api/__init__.py
@@ -5,10 +5,8 @@ from flask import Blueprint
 api = Blueprint(
     'api',
     __name__,
-    subdomain = 'api',
+    template_folder = 'templates',
+    static_folder = 'static'
 )
 
-
-
-from . import authentication, users,  comments,  shares,  users,  find, \
-              likes,  signup,  login,  profile, blog
+from . import authentication, users, comments, shares, users, find, likes
