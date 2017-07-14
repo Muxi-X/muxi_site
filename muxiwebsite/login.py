761a379e2a1332124e3784b8a51264024359955c
yuyilei <yyl1352091742@gmail.com>
2017-07-14 20:03:17 +0800

    update share and blog

---
+++ muxiwebsite/login.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from flask import jsonify, request
+from muxiwebsite.models import User
+from muxiwebsite import db
+
+class Login() :
+    def __init__(self,email,pwd) :
+        self.email = email
+        self.pwd = pwd
+
+    def login(self):
+        user = User.query.filter_by(email=self.email).first()
+        if not user:
+            return jsonify({}), 403
+        if not user.verify_password(self.pwd):
+            return jsonify({}), 400
+        token = user.generate_auth_token()
+        return   token , 200
+
