8d497cfe1d93b306abffe4d0895573d0f108575d
yuyilei <yyl1352091742@gmail.com>
2018-11-13 10:36:29 +0800

    update: add 401 msg

--- muxiwebsite/login.py
+++ muxiwebsite/login.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from flask import jsonify, request ,current_app 
+from flask import jsonify, request ,current_app
 from muxiwebsite.models import User
 from muxiwebsite import db
-import base64 
+import base64
 
 class Login() :
     def __init__(self,username,pwd) :
@@ -15,17 +15,17 @@ class Login() :
         user = User.query.filter_by(username=self.username).first()
         token = " "
         if not user:
-            return jsonify({}), 401
-        if user.verify_password(self.pwd) : 
+            return jsonify({"msg":"no such user"}), 401
+        if user.verify_password(self.pwd) :
             token = user.generate_auth_token()
-            return token , 200 
-        try : 
-            pwd = base64.b64decode(self.pwd) 
-            pwd = unicode(pwd) 
-        except TypeError : 
-            return jsonify({}) , 401 
+            return token , 200
+        try :
+            pwd = base64.b64decode(self.pwd)
+            pwd = unicode(pwd)
+        except TypeError :
+            return jsonify({"msg":"pwd decode error"}) , 401
         if not pwd == current_app.config["MUXI_SECRET_KEY"] :
-            return jsonify({}), 401
+            return jsonify({"msg":"wrong pwd"}), 401
         token = user.generate_auth_token()
-        return   token , 200
+        return  token , 200
 
